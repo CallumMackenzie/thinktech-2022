b06d18676869a18be338b858b78643b5f9da8260
Callum Mackenzie <callum.alex.mackenzie@gmail.com>
2022-11-21 16:53:13 -0800

    Trying to fix create signing urls

--- common/docusign.ts
+++ common/docusign.ts
@@ -114,11 +114,13 @@ export class DocuSignWrapper {
 	async signEnvelopeEmbedded(envelopeDef: EnvelopeDefinition): Promise<Result<string>> {
 		const result = await this.refreshAccessToken();
 		if (result.isError()) return result;
-		if (envelopeDef.sender === undefined)
-			envelopeDef.sender = await
-				this.dsApiClient.getUserInfo(result.result as string) as UserInfo;
+		// if (envelopeDef.sender === undefined)
+			// envelopeDef.sender = await
+				// this.dsApiClient.getUserInfo(result.result as string) as UserInfo;
 
-		const envSummary = await this.envelopesApi.createEnvelope(this.accountId, envelopeDef);
+		const envSummary = await this.envelopesApi.createEnvelope(this.accountId, {
+			envelopeDefinition: envelopeDef
+		});
 		if (envSummary === undefined
 			|| envSummary.status === undefined
 			|| envSummary.envelopeId === undefined
