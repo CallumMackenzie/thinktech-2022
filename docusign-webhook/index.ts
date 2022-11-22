50398c3fc6946a9e91cdc102d24ac3310aaa66b6
Callum Mackenzie <callum.alex.mackenzie@gmail.com>
2022-11-21 23:08:52 -0800

    Diagnostic logs

--- docusign-webhook/index.ts
+++ docusign-webhook/index.ts
@@ -37,10 +37,10 @@ export async function handler(event: any, context: any, callback: any) {
 		Item: convertedItem
 	};
 
-	console.log("Putting item");
+	console.log("Putting item: " + convertedItem);
 	await ddb.putItem(params, (err: any, data: any) => {
-		if (err) return Result.Err(err, 500).result
-		console.log("Data updated: " + data);
+		if (err) console.log("PutItem error: " + err);
+		else console.log("Data updated: " + data);
 	});
 	console.log("Item request finished");
 
